41920acb2436c9392039508ddaa7fac149f2a249
Alejandra Angulo Rico <alangulo@microsoft.com>
2019-04-27 14:59:01 -0500

    Initial commit

---
+++ injectiontest3/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace injectiontest3
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+
+
+        /*  public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+          {
+             // executionContext.GetSection("testing");
+              var test = Program.Configuration;
+              log.WriteLine("test");
+
+              // logger.LogInformation($"{message}\n{executionContext.InvocationId}");
+          }*/
+
+        public static void DownloadStatsFile([TimerTrigger("0 * * * * *", RunOnStartup = true)] TimerInfo timerInfo, TextWriter log)
+        {
+            log.WriteLine("Stats Downloader has started.");
+        }
+
+
+    }
+}
